46b75fa12eeb339f0ce0c949e3608a9cdbd48d55
DmytroRoik <dmytroroik@gmail.com>
2017-12-30 00:18:55 +0200

    write class item

---
+++ js/item.js
@@ -0,0 +1,8 @@
+function Item(options){
+	this.place=options.place;
+	this.price=options.price;
+	this.weight=options.weight;
+
+	this.name=options.name;
+	this.icon=options.icon;
+}
---
+++ js/shop.js
@@ -0,0 +1,4 @@
+var ShopModule=(function(){
+
+
+})();
