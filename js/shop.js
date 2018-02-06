dfc697964f2e8e8ce15b9fd8cf1ee07cb44ee11b
Dimon <dmytroroik@gmail.com>
2018-02-06 02:19:03 +0200

    change modal structure, add style

--- js/shop.js
+++ js/shop.js
@@ -1,66 +1,63 @@
-var ShopModule=(function(){
-	var instance,
-	items=[],
-	selectedItems=[],
-	soldItems=[];
+var ShopModule = (function() {
+    var instance,
+        items = [],
+        selectedItems = [],
+        soldItems = [];
 
-	var createInstance=function () {
-		return {
-			getAllItems: getAllItems,
-			getSelectedItems: getSelectedItems,
-			addItem: addItem,
-			addItemToCart: addItemToCart,
-			removeSelectedItem: removeSelectedItemFromCart,
-			CalcTotalPriceAndWeight:CalcTotalPriceAndWeight,
-			soldItem:soldItem,
-			getSoldItems: getSoldItems,
-		}
-	}
+    var createInstance = function() {
+        return {
+            getAllItems: getAllItems,
+            getSelectedItems: getSelectedItems,
+            addItem: addItem,
+            addItemToCart: addItemToCart,
+            removeSelectedItem: removeSelectedItemFromCart,
+            CalcTotalPriceAndWeight: CalcTotalPriceAndWeight,
+            soldItem: soldItem,
+            getSoldItems: getSoldItems,
+        }
+    }
 
-	var addItem = function (item) {
-		items.push(item);
-	}
+    var addItem = function(item) {
+        items.push(item);
+    }
 
-	var addItemToCart = function (element) {
-    selectedItems.push(element);
-		//alert('items added to cart');
-	}
+    var addItemToCart = function(element) {
+        selectedItems.push(element);
+    }
 
-	var getAllItems=function () {
-		return items;
-	}
-	var getSelectedItems= function(){
-		return selectedItems;
-	}
-	var removeSelectedItemFromCart = function(elem){
-		for(var i=0;i<selectedItems.length;i++){
-			if(selectedItems[i]==elem){
-				selectedItems.splice(i, 1);
-				return;
-			}
-		}
-	}
-	var soldItem=function (item) {
-		soldItems.push(item);
-		removeSelectedItemFromCart(item);
-	}
-	var CalcTotalPriceAndWeight=function(selectedItems){
-		var	totalPrice=0,
-		totalWeight=0;
-		for(var el of selectedItems){
-			totalPrice+= el.price;
-			totalWeight+=el.weight;
-		}
-	return {price: totalPrice, weight: totalWeight};
-	}
+    var getAllItems = function() {
+        return items;
+    }
+    var getSelectedItems = function() {
+        return selectedItems;
+    }
+    var removeSelectedItemFromCart = function(elem) {
+        selectedItems.splice(selectedItems.indexOf(elem), 1)
+    }
+    var soldItem = function(item) {
+        soldItems.push(item);
+        removeSelectedItemFromCart(item);
+    }
+    var CalcTotalPriceAndWeight = function(selectedItems) {
+        if (selectedItems.length === 0) return {
+            price: 0,
+            weight: 0
+        };
+        return selectedItems.reduce(function(prevValue, curValue, index, array) {
+            return {
+                price: prevValue.price + curValue.price,
+                weight: prevValue.weight + curValue.weight
+            }
+        });
+    }
 
-	var getSoldItems=function () {
-		return soldItems;
-	}
-	return {
-		getInstance: function(){
-			return instance|| (instance=createInstance());
-		}
-	}
+    var getSoldItems = function() {
+        return soldItems;
+    }
+    return {
+        getInstance: function() {
+            return instance || (instance = createInstance());
+        }
+    }
 
 })();
